717195c493ecb80c724efb590b0d8606eebfc6f4
LopezR0013 <LopezR013@hotmail.com>
2019-09-23 20:41:01 -0400

    junit

--- src/test/java/test/StatusTest.java
+++ src/test/java/test/StatusTest.java
@@ -6,46 +6,65 @@ import org.hibernate.SessionFactory;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import dao.PostDAOImpl;
 import dao.StatusDAOImpl;
+import dao.UserDAO;
+import dao.UserDAOImpl;
+import model.User;
+import utilities.Helper;
 
 public class StatusTest {
 	
-	@Autowired
-	private SessionFactory sf;
-
-	@Test
-	public void setUserStates() {
-		
-		StatusDAOImpl sImpl = new StatusDAOImpl();
-		assertEquals("Setting Active User Status", "ACTIVE", sImpl.getActiveUser());
-		assertEquals("Setting Admin User Status", "ADMIN", sImpl.getAdminUser());
-		assertEquals("Setting Banned User Status", "BANNED", sImpl.getBannedUser());
-	}
-
-	@Test
-	public void setPostStates() {
-		
-		StatusDAOImpl sImpl = new StatusDAOImpl();
-		assertEquals("Setting Public Post Status", "PUBLIC", sImpl.getPublicPost());
-		assertEquals("Setting Private Post Status", "PRIVATE", sImpl.getPrivatePost());
-		assertEquals("Setting Group Post Status", "GROUP", sImpl.getGroupPost());
-		assertEquals("Setting Banned Post Status", "BANNED", sImpl.getBannedPost());
-	}
-
-	@Test
-	public void setGroupStates() {
-		
-		StatusDAOImpl sImpl = new StatusDAOImpl();
-		assertEquals("Setting Public Group Status", "PUBLIC", sImpl.getPublicGroup());
-		assertEquals("Setting Private Group Status", "PRIVATE", sImpl.getPrivateGroup());
-		assertEquals("Setting Banned Group Status", "BANNED", sImpl.getBannedGroup());
-	}
+//	@Autowired
+//	private SessionFactory sf;
+//	
 	
 	@Test
-	public void setMessageStates() {
-		
-		StatusDAOImpl sImpl = new StatusDAOImpl();
-		assertEquals("Setting Visible Message Status", "VISIBLE", sImpl.getVisibleMessage());
-		assertEquals("Setting Hiden Message Status", "HIDDEN", sImpl.getHiddenMessage());
+	public void seeUsers() {
+		String username = "LopezR013";
+		String password = "Wtwmbiwfil13";
+		User user = Helper.userDAO().getUserByUsername(username);
+		boolean flag = user.checkPassword(password);
+		System.out.println(flag);
+		assertEquals( true , flag);
 	}
+	
+	
+	
+
+//	@Test
+//	public void setUserStates() {
+//		
+//		StatusDAOImpl sImpl = new StatusDAOImpl();
+//		assertEquals("Setting Active User Status", "ACTIVE", sImpl.getActiveUser());
+//		assertEquals("Setting Admin User Status", "ADMIN", sImpl.getAdminUser());
+//		assertEquals("Setting Banned User Status", "BANNED", sImpl.getBannedUser());
+//	}
+//
+//	@Test
+//	public void setPostStates() {
+//		
+//		StatusDAOImpl sImpl = new StatusDAOImpl();
+//		assertEquals("Setting Public Post Status", "PUBLIC", sImpl.getPublicPost());
+//		assertEquals("Setting Private Post Status", "PRIVATE", sImpl.getPrivatePost());
+//		assertEquals("Setting Group Post Status", "GROUP", sImpl.getGroupPost());
+//		assertEquals("Setting Banned Post Status", "BANNED", sImpl.getBannedPost());
+//	}
+//
+//	@Test
+//	public void setGroupStates() {
+//		
+//		StatusDAOImpl sImpl = new StatusDAOImpl();
+//		assertEquals("Setting Public Group Status", "PUBLIC", sImpl.getPublicGroup());
+//		assertEquals("Setting Private Group Status", "PRIVATE", sImpl.getPrivateGroup());
+//		assertEquals("Setting Banned Group Status", "BANNED", sImpl.getBannedGroup());
+//	}
+//	
+//	@Test
+//	public void setMessageStates() {
+//		
+//		StatusDAOImpl sImpl = new StatusDAOImpl();
+//		assertEquals("Setting Visible Message Status", "VISIBLE", sImpl.getVisibleMessage());
+//		assertEquals("Setting Hiden Message Status", "HIDDEN", sImpl.getHiddenMessage());
+//	}
 }
--- src/test/java/test/TypeTest.java
+++ src/test/java/test/TypeTest.java
@@ -8,13 +8,13 @@ import dao.TypeDAOImpl;
 
 public class TypeTest {
 
-	@Test
-	public void setPostTypes() {
-		
-		TypeDAOImpl tImpl = new TypeDAOImpl();
-		assertEquals("Setting Type Text", "TEXT", tImpl.getTextPost());
-		assertEquals("Setting Type Picture", "PICTURE", tImpl.getPicturePost());
-		assertEquals("Setting Type Video", "VIDEO", tImpl.getVideoPost());
-
-	}
+//	@Test
+//	public void setPostTypes() {
+//		
+//		TypeDAOImpl tImpl = new TypeDAOImpl();
+//		assertEquals("Setting Type Text", "TEXT", tImpl.getTextPost());
+//		assertEquals("Setting Type Picture", "PICTURE", tImpl.getPicturePost());
+//		assertEquals("Setting Type Video", "VIDEO", tImpl.getVideoPost());
+//
+//	}
 }
